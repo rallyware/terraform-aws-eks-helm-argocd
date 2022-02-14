49884f3dcbb413fe2034359bebbe46289b8cb40e
SweetOps <vladimir.fess@gmail.com>
2022-02-14 16:56:52 +0200

    feat: initial implementation

--- main.tf
+++ main.tf
@@ -1,3 +1,125 @@
 locals {
-  enabled = module.this.enabled
+  enabled                                     = module.this.enabled
+  region                                      = one(data.aws_region.default[*].name)
+  account_id                                  = one(data.aws_caller_identity.default[*].account_id)
+  eks_cluster_id                              = one(data.aws_eks_cluster.default[*].id)
+  eks_cluster_oidc_issuer_url                 = one(data.aws_eks_cluster.default[*].identity[0].oidc[0].issuer)
+  application_controller_service_account_name = format("%s-application-controller", local.argocd["name"])
+  server_service_account_name                 = format("%s-server", local.argocd["name"])
+  iam_role_enabled                            = local.enabled && local.argocd["create_default_iam_role"]
+  iam_policy_enabled                          = local.iam_role_enabled && local.argocd["create_default_iam_policy"]
+  iam_policy_document                         = local.iam_policy_enabled ? one(data.aws_iam_policy_document.default[*].json) : local.argocd["iam_policy_document"]
+
+  argocd = defaults(var.argocd,
+    {
+      name                       = "argocd"
+      namespace                  = "argo"
+      repository                 = "https://argoproj.github.io/argo-helm"
+      chart                      = "argo-cd"
+      version                    = "3.33.3"
+      max_history                = 10
+      create_namespace           = true
+      dependency_update          = true
+      reuse_values               = false
+      timeout                    = 300
+      override_values            = ""
+      wait                       = true
+      create_default_iam_policy  = true
+      create_default_iam_role    = true
+      iam_policy_document        = ""
+      use_sts_regional_endpoints = true
+    }
+  )
+
+  argocd_helm_values = templatefile("${path.module}/helm-values/argocd.yaml",
+    {
+      fullname_override      = local.argocd["name"]
+      sts_regional_endpoints = local.argocd["use_sts_regional_endpoints"]
+      role_enabled           = local.iam_role_enabled
+      controller_sa_name     = local.application_controller_service_account_name
+      controller_role_arn    = module.application_controller_eks_iam_role.service_account_role_arn
+      server_sa_name         = local.server_service_account_name
+      server_role_arn        = module.server_eks_iam_role.service_account_role_arn
+    }
+  )
+}
+
+data "aws_region" "default" {
+  count = local.enabled ? 1 : 0
+}
+
+data "aws_caller_identity" "default" {
+  count = module.this.enabled ? 1 : 0
+}
+
+data "aws_eks_cluster" "default" {
+  count = module.this.enabled ? 1 : 0
+
+  name = var.eks_cluster_id
+}
+
+data "utils_deep_merge_yaml" "default" {
+  count = local.enabled ? 1 : 0
+
+  input = [
+    local.argocd_helm_values,
+    local.argocd["override_values"]
+  ]
+}
+
+data "aws_iam_policy_document" "default" {
+  count = local.iam_policy_enabled ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    resources = ["arn:aws:iam::${local.account_id}:role/*-argocd-deployer"]
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+module "server_eks_iam_role" {
+  source  = "rallyware/eks-iam-role/aws"
+  version = "0.1.1"
+
+  aws_iam_policy_document     = local.iam_policy_document
+  eks_cluster_oidc_issuer_url = local.eks_cluster_oidc_issuer_url
+  service_account_name        = local.server_service_account_name
+  service_account_namespace   = local.argocd["namespace"]
+
+  enabled = local.iam_role_enabled
+  context = module.this.context
+}
+
+module "application_controller_eks_iam_role" {
+  source  = "rallyware/eks-iam-role/aws"
+  version = "0.1.1"
+
+  aws_iam_policy_document     = local.iam_policy_document
+  eks_cluster_oidc_issuer_url = local.eks_cluster_oidc_issuer_url
+  service_account_name        = local.application_controller_service_account_name
+  service_account_namespace   = local.argocd["namespace"]
+
+  enabled = local.iam_role_enabled
+  context = module.this.context
+}
+
+resource "helm_release" "default" {
+  count = local.enabled ? 1 : 0
+
+  name              = local.argocd["name"]
+  repository        = local.argocd["repository"]
+  chart             = local.argocd["chart"]
+  version           = local.argocd["version"]
+  namespace         = local.argocd["namespace"]
+  max_history       = local.argocd["max_history"]
+  create_namespace  = local.argocd["create_namespace"]
+  dependency_update = local.argocd["dependency_update"]
+  reuse_values      = local.argocd["reuse_values"]
+  wait              = local.argocd["wait"]
+  timeout           = local.argocd["timeout"]
+  values            = [one(data.utils_deep_merge_yaml.default[*].output)]
 }
---
+++ variables.tf
@@ -0,0 +1,77 @@
+variable "eks_cluster_id" {
+  type        = string
+  description = "EKS cluster ID"
+}
+
+variable "argocd" {
+  type = object({
+    name                       = optional(string)
+    namespace                  = optional(string)
+    repository                 = optional(string)
+    chart                      = optional(string)
+    version                    = optional(string)
+    override_values            = optional(string)
+    max_history                = optional(number)
+    create_namespace           = optional(bool)
+    dependency_update          = optional(bool)
+    reuse_values               = optional(bool)
+    wait                       = optional(bool)
+    timeout                    = optional(number)
+    create_default_iam_policy  = optional(bool)
+    create_default_iam_role    = optional(bool)
+    iam_policy_document        = optional(string)
+    use_sts_regional_endpoints = optional(bool)
+  })
+
+  default = {
+    name                       = "argocd"
+    namespace                  = "argo"
+    repository                 = "https://argoproj.github.io/argo-helm"
+    chart                      = "argo-cd"
+    version                    = "3.33.3"
+    max_history                = 10
+    create_namespace           = true
+    dependency_update          = true
+    reuse_values               = false
+    timeout                    = 300
+    override_values            = ""
+    wait                       = true
+    create_default_iam_policy  = true
+    create_default_iam_role    = true
+    iam_policy_document        = ""
+    use_sts_regional_endpoints = true
+  }
+
+  description = <<-DOC
+    name:
+      Release name.
+    chart:
+      Chart name to be installed. 
+    repository:
+      Repository URL where to locate the requested chart.
+    version:
+      Specify the exact chart version to install. If this is not specified, the latest version is installed.
+    namespace:
+      The namespace to install the release into.
+    timeout:
+      Time in seconds to wait for any individual kubernetes operation.
+    reuse_values:
+      When upgrading, reuse the last release's values and merge in any overrides.
+    dependency_update:
+      Runs helm dependency update before installing the chart.
+    create_namespace:
+      Create the namespace if it does not yet exist.
+    wait:
+      Will wait until all resources are in a ready state before marking the release as successful.
+    override_values:
+      A helm values to override.
+    create_default_iam_policy:
+      Whether to create default IAM policy.
+    create_default_iam_role:
+      Whether to create default IAM role.
+    iam_policy_document:
+      Custom IAM policy which will be assigned to IAM role.
+    use_sts_regional_endpoints:
+      Whether to create use STS regional endpoints.
+  DOC
+}
